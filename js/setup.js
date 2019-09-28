4ba2af93bc1c6fe746956671dbfc08e0d0b46c66
kam4atka <sledsib@ya.ru>
2019-09-28 11:14:27 +0300

    #4.0 - Добавляет основные сценарии взаимодействия пользователя с модальным окном настроек

--- js/setup.js
+++ js/setup.js
@@ -1,16 +1,31 @@
 'use strict';
 
+var ESC_CODE = 27;
+var ENTER_CODE = 13;
+
 var WIZARD_COUNT = 4;
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_COATCOLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYECOLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARD_FIREBALLWRAP = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
+var setupPopupOpen = document.querySelector('.setup-open-icon');
 var setupPopup = document.querySelector('.setup');
-setupPopup.classList.remove('hidden');
+var setupPopupForm = setupPopup.querySelector('.setup-wizard-form');
+var setupPopupClose = setupPopup.querySelector('.setup-close');
+var setupPopupUsername = setupPopup.querySelector('.setup-user-name');
+
+var setupPopupWizard = setupPopup.querySelector('.setup-wizard');
+var setupWizardCoat = setupPopupWizard.querySelector('.wizard-coat');
+var setupWizardEyes = setupPopupWizard.querySelector('.wizard-eyes');
+var setupWizardFireballWrap = setupPopup.querySelector('.setup-fireball-wrap');
+var setupWizardFireball = setupWizardFireballWrap.querySelector('.setup-fireball');
+
 var setupSimilar = setupPopup.querySelector('.setup-similar');
 setupSimilar.classList.remove('hidden');
 var setupSimilarList = setupPopup.querySelector('.setup-similar-list');
+var setupPopupSubmit = setupPopup.querySelector('.setup-submit');
 
 var getRandomCount = function (min, max) {
   return Math.ceil(Math.random() * (max - min) + min);
@@ -47,4 +62,73 @@ var getListWizards = function (arr) {
   return fragment;
 };
 
+var openModalHandler = function (element) {
+  element.classList.remove('hidden');
+  document.addEventListener('keydown', escModalHandler);
+};
+
+var closeModalHandler = function (element) {
+  element.classList.add('hidden');
+};
+
+var escModalHandler = function (evt) {
+  if (evt.keyCode === ESC_CODE) {
+    closeModalHandler(setupPopup);
+  }
+};
+
 setupSimilarList.appendChild(getListWizards(generateArrayWizards()));
+// Actions
+setupPopupOpen.addEventListener('click', function () {
+  openModalHandler(setupPopup);
+});
+
+setupPopupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_CODE) {
+    openModalHandler(setupPopup);
+  }
+});
+
+setupPopupClose.addEventListener('click', function () {
+  closeModalHandler(setupPopup);
+});
+
+setupPopupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_CODE) {
+    closeModalHandler(setupPopup);
+  }
+});
+
+setupPopupUsername.addEventListener('keydown', function (evt) {
+  evt.stopPropagation();
+});
+
+setupPopupSubmit.addEventListener('click', function () {
+  setupPopupForm.submit();
+});
+
+var wizardPathColorGenerate = function (element) {
+  if (element.classList.contains('wizard-coat')) {
+    element.style.fill = WIZARD_COATCOLORS[getRandomCount(0, WIZARD_COATCOLORS.length - 1)];
+  }
+  if (element.classList.contains('wizard-eyes')) {
+    element.style.fill = WIZARD_EYECOLORS[getRandomCount(0, WIZARD_EYECOLORS.length - 1)];
+  }
+  if (element.classList.contains('setup-fireball-wrap')) {
+    var fireballWrapColor = WIZARD_FIREBALLWRAP[getRandomCount(0, WIZARD_FIREBALLWRAP.length - 1)];
+    element.style.background = fireballWrapColor;
+    element.querySelector('input').setAttribute('value', fireballWrapColor);
+  }
+};
+
+setupWizardCoat.addEventListener('click', function (evt) {
+  wizardPathColorGenerate(evt.target);
+});
+
+setupWizardEyes.addEventListener('click', function (evt) {
+  wizardPathColorGenerate(evt.target);
+});
+
+setupWizardFireball.addEventListener('click', function () {
+  wizardPathColorGenerate(setupWizardFireballWrap);
+});
